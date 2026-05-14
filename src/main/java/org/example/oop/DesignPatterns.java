3a3547201305c674fa5c7dcda71b2c88a4f07129
farkhod <farkhod.go@gmail.com>
2026-05-14 21:58:16 +0500

    initial commit

---
+++ src/main/java/org/example/oop/DesignPatterns.java
@@ -0,0 +1,7 @@
+package org.example.oop;
+
+public class DesignPatterns {
+    static void main() {
+        
+    }
+}
---
+++ src/main/java/org/example/oop/TypeInference.java
@@ -0,0 +1,16 @@
+package org.example.oop;
+
+public class TypeInference {
+    static void main() {
+        // Type inference is detecting type of expression at compile time
+
+        // var keyword (since Java 10)
+
+        var text = "Hello World!"; // after compile -> String text = "Hello World"
+        System.out.println(text);
+
+        var price = 12.4D; // after compile -> double price = 12.4;
+        System.out.println(price);
+        // price = "A"; // we cannot do it
+    }
+}
